f8580d947b4ad13406ed58bb8129115d0032b083
MithileshGoyal24 <mgoyal@technomatz.com>
2023-11-10 17:56:15 +0530

    propercss

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,4 @@
-$(document).one('click', '.note_div', function(e) {
+$(document).on('click', '.note_div', function(e) {
   e.preventDefault();
     // var noteId = $(this).data('note-id');
     var noteId = $(this).attr('id').split('-')[1];
